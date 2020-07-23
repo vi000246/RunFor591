e0d23290223850ad8c5ac3bf3af35406767a44e9
Yich <07486.yich.lin@ict888.net>
2020-07-23 12:17:25 +0800

    123

---
+++ Entity/SearchModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunFor591.Entity
+{
+    public class SearchModel
+    {
+        public List<string> Kind { get; set; }
+        public string Region { get; set; }
+        public string Section { get; set; }
+        public string RendPrice { get; set; }
+        public string Area { get; set; }
+        public string Order { get; set; }
+        public string HasImg { get; set; }
+        public string Role { get; set; }
+        public string Pattern { get; set; }
+        public string Floor { get; set; }
+        public string Option { get; set; }
+        public string FirstRow { get; set; }
+    }
+}
--- Setting.cs
+++ Setting.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using JsonConfig;
+using Newtonsoft.Json;
+using RunFor591.Entity;
 
 namespace RunFor591
 {
+    //註:Config.User會讀取settings.conf的設定
+    //其他使用方式請參考JsonConfig的說明頁面
     public static class Setting
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         //取得程式執行區間，單位:秒
         public static int GetTimerInterval()
         {
@@ -25,10 +31,30 @@ namespace RunFor591
         }
 
         //取得json db的key
-        //取得house filter的條件
-        public static dynamic GetFilterCondition()
+        public static string GetJsonBinKey()
         {
-            return Config.User.FilterCondition;
+            return "";
+        }
+
+        //取得house filter的條件
+        public static SearchModel GetFilterCondition()
+        {
+            return CastSettingToClass<SearchModel>("SearchModel");
+        }
+
+        private static T CastSettingToClass<T>(string key)
+        {
+            object json;
+            
+            if (((ConfigObject) Config.User).TryGetValue(key, out json))
+            {
+                return JsonConvert.DeserializeObject<T>(json.ToString()); 
+            }
+            else
+            {
+                log.Debug("Cannot CastSettingToClass T:" + nameof(T));
+                return default(T);
+            }
         }
 
         //取得要通知的line的key
