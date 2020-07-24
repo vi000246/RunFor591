700ffcde133f4e7cac4f83f724920e6be22494a5
Yich <07486.yich.lin@ict888.net>
2020-07-24 14:52:52 +0800

    123

---
+++ DataBase/LocationJsonReader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RunFor591.LocationEntity;
+
+namespace RunFor591.DataBase
+{
+    public class LocationJson
+    {
+        public MRTEntity mrtList { get; set; }
+        public RegionEntity regionList { get; set; }
+
+        public LocationJson()
+        {
+            mrtList = this.GetMRTList();
+            regionList = this.GetRegionEntity();
+        }
+
+        public MRTEntity GetMRTList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public RegionEntity GetRegionEntity()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
--- Entity/SearchModel.cs
+++ Entity/SearchModel.cs
@@ -8,17 +8,38 @@ namespace RunFor591.Entity
 {
     public class SearchModel
     {
-        public List<string> Kind { get; set; }
+        public RegionCondition regionCondition { get; set; }
+       
+        public BaseCondition baseCondition { get; set; }
+
+    }
+
+    public class RegionCondition
+    {
+        public List<string> kind { get; set; }
         public List<string> Region { get; set; }
-        public string Section { get; set; }
+        public List<string> Section { get; set; }
+        public List<string> mrtline { get; set; }
+        public List<string> mrtcoods { get; set; }
+    }
+
+    public class BaseCondition
+    {
         public string RendPrice { get; set; }
         public string Area { get; set; }
         public string Order { get; set; }
+        public string OrderType { get; set; }
         public string HasImg { get; set; }
         public string Role { get; set; }
         public string Pattern { get; set; }
+        public string NotCover { get; set; }
         public string Floor { get; set; }
         public string Option { get; set; }
         public string FirstRow { get; set; }
+
     }
+
+
+
+
 }
