75b9379ee5355ca809a90a2626e24c42197df1be
Yich <07486.yich.lin@ict888.net>
2020-07-24 13:31:17 +0800

    123

--- CrawlerUtility/UrlGenerator.cs
+++ CrawlerUtility/UrlGenerator.cs
@@ -14,12 +14,14 @@ namespace RunFor591.CrawlerUtility
         public static string _BaseUrl = "https://rent.591.com.tw/";
         public static string _HouseListUrl = "home/search/rsList";
 
-        //用來取得csrf token跟cookie
+        //591列表頁面 用來取得csrf token跟cookie
         public string GetEntryUrl()
         {
+            //用firstOrDefault 反正只是用來取得token跟cookie
             return UrlGenerator._BaseUrl + QueryStringBuilder().ToList().FirstOrDefault();
         }
 
+        //依據搜尋條件，產生所有要request的api url
         public IEnumerable<string> GetHouseListApiUrl()
         {
             foreach (var queryString in QueryStringBuilder())
@@ -33,16 +35,22 @@ namespace RunFor591.CrawlerUtility
         {
             List<string> returnParams = new List<string>();
             var filter = Setting.GetFilterCondition();
+            ValidateFilterCondition(filter);
+
             PropertyInfo[] properties = typeof(SearchModel).GetProperties();
             foreach (PropertyInfo property in properties)
             {
                 var settingValue = GetPropValue(filter,property.Name);
-                //kind是用來判斷租屋類型 1:整層住家 2:獨立套房 3:分租套房 這個需要分開request
-                if (property.Name != "Kind")
+                //kind是用來判斷租屋類型 1:整層住家 2:獨立套房 3:分租套房
+                //region是縣市，kind跟region都不能多選,必須loop產出request url
+                if (property.Name != "Kind" && property.Name != "Region")
                 {
                     returnParams.Add(String.Format("{0}={1}", property.Name, settingValue));
                 }
             }
+
+            //loop縣市、鄉鎮、捷運
+            //如果有選捷運，依據捷運線所屬哪個捷運，組出mrt參數(對應到region 桃捷=6 北捷=1 高雄捷運=17)
 
             var qs = String.Join("&", returnParams.ToArray());
             foreach (var kind in filter.Kind)
@@ -50,6 +58,15 @@ namespace RunFor591.CrawlerUtility
                 yield return "?kind=" + kind + qs;
             }
             
+        }
+
+        private void ValidateFilterCondition(SearchModel filter)
+        {
+            //判斷是否有選擇region
+            //如果有選擇鄉鎮section，判斷是否在region底下
+            //如果有選擇捷運線，判斷是否在region底下
+            //如果有選擇捷運線，捷運站為必選
+            //判斷捷運站是否在捷運線底下
         }
 
         private static object GetPropValue(object src, string propName)
---
+++ LocationEntity/RegionEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunFor591.LocationEntity
+{
+    public class RegionEntity
+    {
+        public List<Region> region { get; set; }
+    }
+    public class Section
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+
+    }
+
+    public class Region
+    {
+        public int id { get; set; }
+        public string txt { get; set; }
+        public List<Section> section { get; set; }
+
+    }
+}
