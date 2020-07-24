3d2da9daa583a091a6cc23589305ff924700778e
Yich <07486.yich.lin@ict888.net>
2020-07-24 13:31:17 +0800

    123

---
+++ LocationEntity/MRTEntity.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunFor591.LocationEntity
+{
+    public class MRTEntity
+    {
+        public List<TaipeiMrt> taipei_mrt { get; set; }
+        public List<KaohsiungMrt> kaohsiung_mrt { get; set; }
+        public List<TaoyuanMrt> taoyuan_mrt { get; set; }
+
+    }
+    public class Station
+    {
+        public string sid { get; set; }
+        public string name { get; set; }
+        public string lat { get; set; }
+        public string lng { get; set; }
+        public string zoom { get; set; }
+        public string nid { get; set; }
+
+    }
+
+    public class TaipeiMrt
+    {
+        public string lat { get; set; }
+        public string lng { get; set; }
+        public string name { get; set; }
+        public string sid { get; set; }
+        public string zoom { get; set; }
+        public List<Station> station { get; set; }
+
+    }
+
+    public class Station2
+    {
+        public string sid { get; set; }
+        public string name { get; set; }
+        public string lat { get; set; }
+        public string lng { get; set; }
+        public string zoom { get; set; }
+        public string nid { get; set; }
+
+    }
+
+    public class KaohsiungMrt
+    {
+        public string lat { get; set; }
+        public string lng { get; set; }
+        public string name { get; set; }
+        public string sid { get; set; }
+        public string zoom { get; set; }
+        public List<Station2> station { get; set; }
+
+    }
+
+    public class Station3
+    {
+        public string sid { get; set; }
+        public string name { get; set; }
+        public string lat { get; set; }
+        public string lng { get; set; }
+        public string zoom { get; set; }
+        public string nid { get; set; }
+
+    }
+
+    public class TaoyuanMrt
+    {
+        public string lat { get; set; }
+        public string lng { get; set; }
+        public string name { get; set; }
+        public string sid { get; set; }
+        public string zoom { get; set; }
+        public List<Station3> station { get; set; }
+
+    }
+
+
+}
